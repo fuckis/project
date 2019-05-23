424018104661edeaf7c5bf1a02bcc338c0d44883
fuckis <13545153171@sina.cn>
2019-05-23 16:23:57 +0800

    项目结构调整

---
+++ project-web/project-consul-web/src/main/java/web/Application.java
@@ -0,0 +1,15 @@
+package web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by haddis on 2019/5/22.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
