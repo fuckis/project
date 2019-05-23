4f15e17eda2daffef00921420db409a90a918a49
fuckis <13545153171@sina.cn>
2019-05-23 12:46:45 +0800

    docker部署spring boot项目的相关脚本

---
+++ docker/docker-jdk-mvn-git/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+nohup /usr/local/bin/dockerd --host=unix:///var/run/docker.sock --host=tcp://127.0.0.1:2375 --storage-driver=overlay&
+timeout -t 15 sh -c "until docker info; do echo .; sleep 1; done"
+
+git clone https://github.com/fuckis/project.git
+
+cd project
+
+mvn clean
+mvn install
