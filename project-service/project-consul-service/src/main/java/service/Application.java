2548b21fa2326150c1da2ff9fcbd00746cb9df0e
fuckis <13545153171@sina.cn>
2019-05-24 13:14:05 +0800

    consul服务发现与注册

--- project-service/project-consul-service/src/main/java/service/Application.java
+++ project-service/project-consul-service/src/main/java/service/Application.java
@@ -10,7 +10,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableAutoConfiguration
 public class Application {
 
     public static void main(String[] args) {
--- project-web/project-consul-web/src/main/java/web/Application.java
+++ project-web/project-consul-web/src/main/java/web/Application.java
@@ -2,11 +2,13 @@ package web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Created by haddis on 2019/5/22.
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Application {
 
     public static void main(String[] args) {
