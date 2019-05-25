642b3744b60fb809e7f08b9bd7233b86c87da78a
fuckis <13545153171@sina.cn>
2019-05-25 16:57:33 +0800

    搭建eureka注册中心

---
+++ project-eureka-server/src/main/java/server/Application.java
@@ -0,0 +1,18 @@
+package server;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by haddis on 2019/5/22.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
