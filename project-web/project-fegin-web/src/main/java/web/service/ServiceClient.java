f74e29c1badf80278098801a2499d83fee2acac0
fuckis <13545153171@sina.cn>
2019-05-28 11:13:17 +0800

    eureka + regin + retry

--- project-web/project-fegin-web/src/main/java/web/service/ServiceClient.java
+++ project-web/project-fegin-web/src/main/java/web/service/ServiceClient.java
@@ -1,11 +1,13 @@
 package web.service;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  * Created by haddis on 2019/5/28.
  */
+@Retryable
 @FeignClient("EUREKA-SERVICE")
 public interface ServiceClient {
 
