8248c28558e5bf203ae0136a2a056658010a4624
fuckis <13545153171@sina.cn>
2019-05-28 16:18:49 +0800

    eureka + feign

--- project-web/project-fegin-web/src/main/java/web/service/ServiceClient.java
+++ project-web/project-fegin-web/src/main/java/web/service/ServiceClient.java
@@ -1,16 +1,26 @@
 package web.service;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.retry.annotation.Retryable;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  * Created by haddis on 2019/5/28.
  */
-@Retryable
-@FeignClient("EUREKA-SERVICE")
+@FeignClient(value = "EUREKA-SERVICE",
+        fallback = ServiceFallbackClient.class
+)
 public interface ServiceClient {
 
     @GetMapping("/service/test")
     String test();
+}
+
+@Component
+class ServiceFallbackClient implements ServiceClient {
+
+    @Override
+    public String test() {
+        return "test";
+    }
 }
